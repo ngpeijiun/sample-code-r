04bcd8c4f2d364dbdd9e4dc94283ee4ab2a88b8b
ngpeijiun <ngpeijiun@gmail.com>
2018-06-22 11:00:05 +0800

    Create 07-read-file.R

---
+++ 07-read-file.R
@@ -0,0 +1,21 @@
+HousePrice <- read.table("houses.data")
+# HousePrice <- read.table("houses.data", header = TRUE)
+
+# inp <- scan("input.dat", list("", 0, 0))
+# label <- inp[[1]]; x <- inp[[2]]; y <- inp[[3]]
+# inp <- scan("input.dat", list(id = "", x = 0, y = 0)
+# label <- inp$id; x <- inp$x; y <- inp$y
+
+# X <- matrix(scan("light.dat", 0), ncol = 5, byrow = TRUE)
+
+# data()
+data(infert)
+
+# data(package = "rpart")
+data(Puromycin, package="datasets")
+
+# xnew <- edit(xold)
+# xold <- edit(xold)
+# fix(xold)
+
+# xnew <- edit(data.frame())
