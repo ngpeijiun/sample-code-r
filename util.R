01fb395d61f1fcb384ff65ef2bdb0bdd220758a8
ngpeijiun <ngpeijiun@gmail.com>
2018-06-28 09:53:25 +0800

    Update util.R

--- util.R
+++ util.R
@@ -11,6 +11,13 @@ normalEquation <- function(X, y) {
   as.vector(solve(t(X) %*% X) %*% t(X) %*% y)
 }
 
+featureTable <- function(df, key) {
+  t <- table(as.numeric(rownames(df)), BreastCancer[[key]])
+  df <- as.data.frame.matrix(t)
+  names(df) <- paste(key, levels(BreastCancer[[key]]), sep = ".")
+  df
+}
+
 featureNormalize <- function(X) {
   if (is.vector(X)) {
     mu <- mean(X)
@@ -41,23 +48,56 @@ featureDescaling <- function(X, conf) {
   }
 }
 
-computeCost <- function(X, y, theta) {
+computeCostLinear <- function(theta, X, y) {
   m <- length(y)
   prediction <- as.vector(X %*% theta)
-  1 / (2 * m) * sum((prediction - y) ^ 2)
+  cost <- (prediction - y) ^ 2
+  J <- 1 / (2 * m) * sum(cost)
 }
 
-computeDelta <- function(X, y, theta) {
+computeCostLogistic <- function(theta, X, y) {
+  m <- length(y)
+  prediction <- as.vector(sigmoid(X %*% theta))
+  cost <- -1 * (y * log(prediction) + (1 - y) * log(1 - prediction))
+  J <- 1 / m * sum(cost)
+}
+
+computeCost <- function(theta, X, y, model = "linear") {
+  f <- switch(model,
+              linear = computeCostLinear,
+              logistic = computeCostLogistic)
+  f(theta, X, y)
+}
+
+computeDeltaLinear <- function(theta, X, y) {
   m <- length(y)
   prediction <- as.vector(X %*% theta)
-  as.vector(1 / m * t(X) %*% (prediction - y))
+  grad <- as.vector(1 / m * t(X) %*% (prediction - y))
 }
 
-gradientDescent <- function(X, y, alpha = 0.1, momentum = 0, accelerated = FALSE, numIter = 10, numCost = 10) {
+computeDeltaLogistic <- function(theta, X, y) {
+  m <- length(y)
+  prediction <- as.vector(sigmoid(X %*% theta))
+  grad <- as.vector(1 / m * t(X) %*% (prediction - y))
+}
+
+computeDelta <- function(theta, X, y, model = "linear") {
+  f <- switch(model,
+              linear = computeDeltaLinear,
+              logistic = computeDeltaLogistic)
+  f(theta, X, y)
+}
+
+gradientDescent <- function(X, y,
+                            alpha = 0.1, momentum = 0, accelerated = FALSE,
+                            numIter = 10, numCost = 10,
+                            model = "linear") {
+
   n <- ncol(X)
   theta <- numeric(n)
   velocity <- numeric(n)
 
+  numCost <- min(numIter, numCost)
   costHistory <- numeric(numCost)
   j <- 0
 
@@ -66,37 +106,41 @@ gradientDescent <- function(X, y, alpha = 0.1, momentum = 0, accelerated = FALSE
       theta <- theta - momentum * velocity
     }
 
-    velocity <- momentum * velocity + alpha * computeDelta(X, y, theta)
+    velocity <- momentum * velocity + alpha * computeDelta(theta, X, y, model)
     theta <- theta - velocity
 
     if (floor(i * numCost / numIter) > j) {
       j <- floor(i * numCost / numIter)
-      costHistory[j] = computeCost(X, y, theta)
+      costHistory[j] <- computeCost(theta, X, y, model)
     }
   }
 
   list(theta = theta, costHistory = costHistory, velocity = norm(matrix(momentum * velocity)))
 }
 
-steepestDescent <- function(X, y, maxIter = 10, tolerance = 1e-12) {
+steepestDescent <- function(X, y,
+                            tolerance = 1e-12,
+                            maxIter = 10,
+                            model = "linear") {
+
   n <- ncol(X)
   theta <- numeric(n)
 
-  ySym <- t(X) %*% y
-  XSym <- t(X) %*% X
   costHistory <- numeric(0)
+  last <- 0
 
   for (i in 1:maxIter) {
-    residual <- as.vector(ySym - XSym %*% theta)
-    alpha <- as.numeric(t(residual) %*% residual) / as.numeric(t(residual) %*% XSym %*% residual)
-    theta <- theta + alpha * residual
+    grad <- computeDelta(theta, X, y, model)
+    theta <- theta - grad
 
-    costHistory[i] <- computeCost(X, y, theta)
+    costHistory[i] <- computeCost(theta, X, y, model)
 
-    if (norm(matrix(residual)) < tolerance) {
+    error <- norm(matrix(grad))
+    if (abs(error - last) < tolerance) {
       break
     }
+    last <- error
   }
 
-  list(theta = theta, costHistory = costHistory, error = norm(matrix(residual)))
+  list(theta = theta, costHistory = costHistory, error = abs(error - last))
 }
