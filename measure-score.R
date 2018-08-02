93859529ea8663647eb69815e1caa49b1d5854b3
ngpeijiun <ngpeijiun@gmail.com>
2018-08-02 15:45:30 +0800

    Update measure-score.R

--- measure-score.R
+++ measure-score.R
@@ -5,9 +5,9 @@ score <- matrix(0, 4, ncol(Y))
 colnames(score) <- names(Y)
 rownames(score) <- c("precision", "recall", "F1 Score", "F1 Score (All-True)")
 
-for (i in 1:ncol(Y)) {
+for (i in 1:nrow(Y)) {
   score["precision", i] <- precision(prediction_round[, i], Y[, i])
   score["recall", i] <- recall(prediction_round[, i], Y[, i])
   score["F1 Score", i] <- F1Score(prediction_round[, i], Y[, i])
-  score["F1 Score (All-True)", i] <- F1Score(matrix(1, m, 1), Y[, i])
+  score["F1 Score (All-True)", i] <- F1Score(matrix(1, nrow(Y), 1), Y[, i])
 }
